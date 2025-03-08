3ffa2142334fc1c71f47a7d0cd7fb3e22c02da2a
trazer-creations <68105004+trazer-creations@users.noreply.github.com>
2025-03-08 22:34:58 +0530

    Update install-fzf.sh

--- .devcontainer/install-fzf.sh
+++ .devcontainer/install-fzf.sh
@@ -5,3 +5,12 @@ git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 
 # Run the fzf installation script
 ~/.fzf/install --all
+
+# Add Starship initialization to shell configuration files
+if [ -f ~/.bashrc ]; then
+    echo 'eval "$(starship init bash)"' >> ~/.bashrc
+fi
+
+if [ -f ~/.zshrc ]; then
+    echo 'eval "$(starship init zsh)"' >> ~/.zshrc
+fi
